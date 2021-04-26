8bbbec04523c81efd0c36d63d737a27f1aa0e0ca
Ben Kallus <bkallus@hamilton.edu>
2021-04-26 13:18:22 -0400

    added a gitignore; removed status bar text

--- dwm.c
+++ dwm.c
@@ -1996,8 +1996,6 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
-	// if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-	//  	strcpy(stext, "dwm-"VERSION);
 	drawbar(selmon);
 }
 
